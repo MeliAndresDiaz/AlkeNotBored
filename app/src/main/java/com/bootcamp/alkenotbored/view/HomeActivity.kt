42ef17d7c3ed76b706bf93d5708f09e20a475993
MeliAndresDiaz <andres.diaz@mercadolibre.com.co>
2022-09-05 23:10:06 -0500

    Initial commit

---
+++ app/src/main/java/com/bootcamp/alkenotbored/view/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.bootcamp.alkenotbored.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bootcamp.alkenotbored.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+
+
+}
