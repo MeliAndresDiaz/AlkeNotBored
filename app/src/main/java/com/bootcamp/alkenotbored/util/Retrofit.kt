6d91242a7d3b15d9cf322cf1f577ed21bb5b34d3
MeliAndresDiaz <andres.diaz@mercadolibre.com.co>
2022-09-06 22:58:50 -0500

    Update logic on project
    - Update logic and views on HomeActivity and TermsAndConditionsActivity
    - Extract Strings on app
    - Add Navigation with Intent extension

--- app/src/main/java/com/bootcamp/alkenotbored/util/Retrofit.kt
+++ app/src/main/java/com/bootcamp/alkenotbored/util/Retrofit.kt
@@ -1,10 +1,18 @@
 package com.bootcamp.alkenotbored.util
 
-import com.bootcamp.alkenotbored.utils.Constants.BASE_URL
+import android.app.Activity
+import android.util.Log
+import com.bootcamp.alkenotbored.R
+import com.bootcamp.alkenotbored.utils.showAlert
+import com.example.notbored.APIService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit {
+    private val BASE_URL = "http://www.boredapi.com/api/"
 
     /**
      * This instance of Retrofit will have the base url of the endpoint, it will be in charge of
@@ -25,4 +33,44 @@ class Retrofit {
      * will be executed in an asynchronous thread.
      *
      */
+
+    private fun getActivity(
+        participants: Int? = null,
+        type: String? = null,
+        price: Double? = null,
+        activity: Activity
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            //API call and body response
+            val call = getRetrofit().create(APIService::class.java)
+                .getActivity("activity/", participants, type, price)
+            val activityResponse = call.body()
+
+            //As we're going to show the result visually, we must execute it in the UI thread. For that, we use runOnUiThread
+            activity.runOnUiThread {
+                if (call.isSuccessful) {
+                    activityResponse?.let { response ->
+                        //If no activity is available, show an error message
+                        if (response.error.isNotEmpty()) {
+                            activity.showAlert(
+                                R.string.global_alert_text_title,
+                                messageId = response.error
+                            )
+                        }
+
+                        /** TODO: Aplicar la logica para ir a la proxima pantalla con los datos requeridos
+                         *  activity.type, activity.activity, activity.price, activity.participants
+                         */
+                        Log.d("Retrofit", response.toString())
+                    }
+                } else {
+                    //Call failed or not successful. We show an error message
+                    activity.showAlert(
+                        R.string.global_alert_text_title,
+                        messageId = "An error has occurred with API. Please try again."
+                    )
+                }
+            }
+        }
+    }
 }
